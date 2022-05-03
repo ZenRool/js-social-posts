d0bb1658ce1f50d24ab7a1c70e55eb352be1f626
Zenrool <lorenzopapperini@hotmail.it>
2022-05-03 15:54:53 +0200

    Scaffolding

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    }
+];
+// posts 
+// id: n + 1
+// content: lorem testo del post
+// author: Nome e foto autore foto del profilo
+// created: data in formato americano (mm-gg-yyyy: es 05-03-2022)
+// likes: numero di likes
+// media: non tutti i post devono avere l'immagine
+
+// immagine -> https://unsplash.it/300/300?image=<id>
